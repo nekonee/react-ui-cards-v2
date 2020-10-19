3ea746fbc4c086b51883d97260666c12d264ba6c
nekonee <nekonee@protonmail.com>
2020-10-19 10:17:33 +0200

    PaymentCard base

---
+++ src/cards/PaymentCard/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import cx from 'classnames';
+
+
+import { Card } from '../Card';
+import { FlippingCard } from '../FlippingCard';
+import { FlippingCardFront } from '../FlippingCard/FlippingCardFront';
+import { FlippingCardBack } from '../FlippingCard/FlippingCardBack';
+import { PaymentCardBackground } from './PaymentCardBackground';
+import { IssuerIcon } from './IssuerIcon';
+import { PaymentCardNumber } from './PaymentCardNumber';
+import { Chip } from './Chip';
+import { PaymentCardDate } from './PaymentCardDate';
+import { PaymentCardName } from './PaymentCardName';
+import { PaymentCardBack } from './PaymentCardBack';
+
+import styles from './styles.module.scss';
+
